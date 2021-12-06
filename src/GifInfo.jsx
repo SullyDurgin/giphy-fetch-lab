2a3a2d05feb9f8750956cfa885958276fc1bd91e
SullyDurgin <katsdurgin@gmail.com>
2021-12-06 18:12:05 -0500

    not working but closer

--- src/GifInfo.jsx
+++ src/GifInfo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const GifInfo = ({ gif }) => {
   return (
     <>
-    <iframe src="https://giphy.com/embed/WxDZ77xhPXf3i" width="480" height="258" frameBorder="0" class="giphy-embed" title='gif' allowFullScreen></iframe>
+    <img src={gif.data.images.original.url} alt="gif" />
     </>
   )
 }
