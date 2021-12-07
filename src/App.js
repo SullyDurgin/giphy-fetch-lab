db6fc0d149ca5f7bd433b0c24716454293f1e7bc
SullyDurgin <katsdurgin@gmail.com>
2021-12-07 09:47:51 -0500

    really done

--- src/App.js
+++ src/App.js
@@ -4,39 +4,40 @@ import React, { useEffect, useState } from 'react';
 import Form from './Form';
 
 function App() {
-  const [gifData, setGifData] = useState({data:null})
-  const [gifTerm, setGifTerm] = useState('')
+	const [gifData, setGifData] = useState({ data: null })
+	const [gifTerm, setGifTerm] = useState('')
 
-  const handleSubmit = term => {
-    setGifTerm(term)
-  }
+	const handleSubmit = (term) => {
+		setGifTerm(term)
+		console.log('gifTerm - app', term)
+	}
 
-  useEffect(() => {
-    let gifUrl = `https://api.giphy.com/v1/gifs/search?api_key=LFYMIdgkEwDs3KuyUKvtYuLr4CleMVPQ&q=${gifTerm}&limit=1`
-    async function makeApiCall() {
-      try {
-        const res = await fetch(gifUrl)
-        const data = await res.json()
-        setGifData((data))
-      } catch(error) {
-      }
-    }
-    makeApiCall()
-  }, [gifTerm])
-  
-  const handleRefresh = () => {
-    setGifData({data:null})
-  }
+	useEffect(() => {
+		let gifUrl = `https://api.giphy.com/v1/gifs/search?api_key=LFYMIdgkEwDs3KuyUKvtYuLr4CleMVPQ&q=${gifTerm}&limit=1`
+		async function makeApiCall() {
+			try {
+				const res = await fetch(gifUrl)
+				const data = await res.json()
+				setGifData(data.data)
+			} catch (error) {
+				console.log(error)
+			}
+		}
+		makeApiCall()
+	}, [gifTerm])
 
-  return (
-    <div className="App">
-      <h1>Giphy</h1>
-      <Form handleSubmit={handleSubmit}/>
-      <button onClick = {() => handleRefresh()}>Make API Call Again</button>
-      {gifData.data ? 
-      <GifInfo  gif={gifData}/> : null }
-    </div>
-  );
+	const handleRefresh = () => {
+		setGifData({ data: null })
+	}
+	console.log(gifData)
+	return (
+		<div className='App'>
+			<h1>Giphy</h1>
+			<Form handleSubmit={handleSubmit} />
+			<button onClick={() => handleRefresh()}>Make API Call Again</button>
+			{gifData.length ? <GifInfo gif={gifData} /> : null}
+		</div>
+	)
 }
 
 export default App;
