39f9a85b919d37ad2a0dc6f5d793ec4a5c9094a7
SullyDurgin <katsdurgin@gmail.com>
2021-12-07 09:21:58 -0500

    almost done but idk how to get my own api key :(

--- src/App.js
+++ src/App.js
@@ -1,42 +1,46 @@
-import './App.css'
-import GifInfo from './GifInfo'
-import React, { useEffect, useState } from 'react'
-import Form from './Form'
+import './App.css';
+import GifInfo from './GifInfo';
+import React, { useEffect, useState } from 'react';
+import Form from './Form';
 
 function App() {
-	const [gifData, setGifData] = useState('')
-	const [gifTerm, setGifTerm] = useState('')
+  const [gifData, setGifData] = useState({data:null})
+  const [gifTerm, setGifTerm] = useState('')
 
-	const handleSubmit = (term) => {
-		setGifTerm(term)
-		console.log('gifTerm - app', term)
-	}
+  const handleSubmit = term => {
+    setGifTerm(term)
+    console.log('gifTerm - app', term)
+  }
 
-	useEffect(() => {
-		let gifUrl = `https://api.giphy.com/v1/gifs/search?api_key=LFYMIdgkEwDs3KuyUKvtYuLr4CleMVPQ&q=${gifTerm}&limit=1`
-		async function makeApiCall() {
-			try {
-				console.log(gifUrl)
-				const res = await fetch(gifUrl)
-				console.log(res)
-				const data = await res.json()
-				console.log(data)
-				setGifData(data)
-			} catch (error) {
-				console.log(error)
-			}
-		}
-		makeApiCall()
-	}, [gifTerm])
+  useEffect(() => {
+    let gifUrl = `https://api.giphy.com/v1/gifs/search?api_key=LFYMIdgkEwDs3KuyUKvtYuLr4CleMVPQ&q=${gifTerm}&limit=1`
+    async function makeApiCall() {
+      try {
+        console.log(gifUrl)
+        const res = await fetch(gifUrl)
+        console.log(res)
+        const data = await res.json()
+        setGifData((data))
+      } catch(error) {
+        console.log(error)
+      }
+    }
+    makeApiCall()
+  }, [gifTerm])
+  
+  const handleRefresh = () => {
+    setGifData({data:null})
+  }
 
-	return (
-		<div className='App'>
-			<h1>Giphy</h1>
-			<Form handleSubmit={handleSubmit} />
-			<button>Make API Call Again</button>
-			<GifInfo gif={gifData} />
-		</div>
-	)
+  return (
+    <div className="App">
+      <h1>Giphy</h1>
+      <Form handleSubmit={handleSubmit}/>
+      <button onClick = {() => handleRefresh()}>Make API Call Again</button>
+      {gifData.data ? 
+      <GifInfo  gif={gifData}/> : null }
+    </div>
+  );
 }
 
-export default App
+export default App;
